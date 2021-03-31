20210f3f7a6c673aa4ce7ca2ecd4c4ccad680ac5
aashishpeepra <ironhack082327@gmail.com>
2021-03-31 23:29:24 +0530

    Added bundle

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const server = express();
 const userRoutes = require("./routes/user.routes");
 const mailRoutes = require("./routes/mail.routes");
 const cors = require("cors");
-
+const path = require("path")
 const mongoose = require("mongoose");
 
 const PORT = process.env.PORT || 5000;
@@ -16,12 +16,13 @@ server.use(express.urlencoded({ extended: true }));
 server.use(cors())
 server.use("/api/user",userRoutes)
 server.use("/api/mail",mailRoutes)
-server.use("/",(req,res,next)=>{
-    res.status(201).json({
-        message:"Listening at "+PORT
-    })
-})
 
+server.use(express.static(path.join(__dirname, 'build')));
+
+
+server.get('/*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+  });
 mongoose.connect(process.env.MONGO_URL,{
     useNewUrlParser:true,
     useUnifiedTopology: true,
